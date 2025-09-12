f486e95b13c8c5aebd7ac607ba1e2f3718c969ce
itsdevkunal <itskunaltoday@gmail.com>
2025-09-12 10:41:38 +0530

    new navbar for mobile

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,11 @@
 'use client';
 import Link from 'next/link';
 import styles from './navbar.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
   const router = useRouter();
   useEffect(() => {
     // Scroll to the target with an offset when the page loads
@@ -34,14 +35,22 @@ export default function Navbar() {
   };
 
   return (
-    <nav className={styles.navbar}>
-      <img className={styles.mypic} src="/pp.png" alt="mypic" />
-      <ul>
-        <li><Link href='/'>Home</Link></li>
-        <li><a onClick={(e) => handleLinkClick(e, '#about')}>About</a></li>
-        <li><a onClick={(e) => handleLinkClick(e, '#skills')}>Skills</a></li>
-        <li><Link href='/contact'>Contact</Link></li>
-      </ul>
-    </nav>
+    <>
+      <div className={`${styles.background} ${toggle ? styles.backgroundOpen : ''}`} onClick={() => setToggle(false)}></div>
+      <div className={styles.hamburger} onClick={() => setToggle(!toggle)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      <nav className={`${styles.navbar} ${toggle ? styles.navbarOpen : ''}`}>
+        <img className={styles.mypic} src="/pp.png" alt="mypic" />
+        <ul>
+          <li><Link href='/'>Home</Link></li>
+          <li><a onClick={(e) => handleLinkClick(e, '#about')}>About</a></li>
+          <li><a onClick={(e) => handleLinkClick(e, '#skills')}>Skills</a></li>
+          <li><Link href='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
+    </>
   );
 }
